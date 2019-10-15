82fac8a5b3d98b6650b13bc36067dd321b3660c8
sychsergiy <sychsergiy@gmail.com>
2019-10-15 15:15:57 +0300

    privacy request added

--- awsgi_poc/server/main/views.py
+++ awsgi_poc/server/main/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
 main_blueprint = Blueprint("main", __name__)
 
@@ -6,3 +6,13 @@ main_blueprint = Blueprint("main", __name__)
 @main_blueprint.route("/")
 def index():
     return jsonify({"response": "ok"})
+
+
+@main_blueprint.route("/privacy/v1/request", methods=["POST"])
+def privacy_request():
+    request_id = request.json["requestConfiguration"]["requestId"]
+
+    if request_id == "test_ID":
+        return jsonify({"response": "ok"})
+
+    return {"error": "Invalid requestId"}, 400
--- awsgi_poc/tests/main/test_views.py
+++ awsgi_poc/tests/main/test_views.py
@@ -1,5 +1,87 @@
+import pytest
+from flask import url_for
+
+
 def test_home(client):
     r = client.get("/")
 
     assert r.status_code == 200
     assert r.json == {"response": "ok"}
+
+
+@pytest.fixture
+def correct_request_body():
+    request_body = {
+        "requestConfiguration": {
+            "requestId": "test_ID",
+            "subTaskId": [],
+            "subjectType": {},
+            "dataPrivacyActId": {},
+            "assetInsightId": [],
+            "requestType": {},
+        },
+        "userData": {
+            "userName": "",
+            "firstName": "",
+            "middleName": "",
+            "lastName": "",
+            "suffix": "",
+            "emailAddress": "",
+            "country": "",
+            "zip": "",
+            "address": "",
+            "state": "",
+            "city": "",
+            "apartmentSuiteNumber": "",
+            "ssn": "",
+            "dateOfBirth": "",
+            "telephoneNumber": "",
+        },
+    }
+    return request_body
+
+
+@pytest.fixture
+def wrong_request_body():
+    request_body = {
+        "requestConfiguration": {
+            "requestId": "",
+            "subTaskId": [],
+            "subjectType": {},
+            "dataPrivacyActId": {},
+            "assetInsightId": [],
+            "requestType": {},
+        },
+        "userData": {
+            "userName": "",
+            "firstName": "",
+            "middleName": "",
+            "lastName": "",
+            "suffix": "",
+            "emailAddress": "",
+            "country": "",
+            "zip": "",
+            "address": "",
+            "state": "",
+            "city": "",
+            "apartmentSuiteNumber": "",
+            "ssn": "",
+            "dateOfBirth": "",
+            "telephoneNumber": "",
+        },
+    }
+    return request_body
+
+
+def test_privacy_request_ok(client, correct_request_body):
+    r = client.post(url_for("main.privacy_request"), json=correct_request_body)
+
+    assert r.status_code == 200
+    assert r.json == {"response": "ok"}
+
+
+def test_privacy_request_400(client, wrong_request_body):
+    r = client.post(url_for("main.privacy_request"), json=wrong_request_body)
+
+    assert r.status_code == 400
+    assert r.json == {"error": "Invalid requestId"}
